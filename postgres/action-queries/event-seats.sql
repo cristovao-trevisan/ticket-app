dbb105e6610750b171f616f936014a985af7b1c9
Cristóvão Trevisan <cristovao.trevisan@gmail.com>
2019-05-05 19:04:33 -0300

    Add generic seats to event-seats query

--- postgres/action-queries/event-seats.sql
+++ postgres/action-queries/event-seats.sql
@@ -11,6 +11,19 @@ WITH
     FROM "SeatFixtureAreas"
     GROUP BY event
   ),
+  genericSeats AS (
+    SELECT
+      event,
+      JSON_AGG(JSON_BUILD_OBJECT(
+        'id', id,
+        'name', name,
+        'capacity', capacity,
+        'reserved', reserved,
+        'occupied', occupied
+      )) AS "genericSeats"
+    FROM ONLY "GenericSeats"
+    GROUP BY event
+  ),
   seatAreas AS (
     SELECT
       event,
@@ -63,11 +76,13 @@ SELECT
   event.name,
   event.description,
   "fixtureAreas",
+  "genericSeats",
   "seatAreas",
   "numberedSeats",
   pricings.pricings
 FROM "Events" AS event
 LEFT JOIN fixtureAreas ON fixtureAreas.event = event.id
+LEFT JOIN genericSeats ON genericSeats.event = event.id
 LEFT JOIN seatAreas ON seatAreas.event = event.id
 LEFT JOIN numberedSeats ON numberedSeats.event = event.id
 LEFT JOIN pricings ON pricings.event = event.id
