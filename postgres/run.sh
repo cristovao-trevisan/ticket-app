418955b40bb13349479cceef9c9ce6651d1f6b37
Cristóvão Trevisan <cristovao.trevisan@gmail.com>
2019-05-08 22:29:23 -0300

    Fix run script to use volume

--- postgres/run.sh
+++ postgres/run.sh
@@ -1,1 +1,1 @@
-docker run --rm --name pg-docker -e POSTGRES_PASSWORD=$DB_PASSWORD -d -p 5432:5432 -v $HOME/docker/volumes/postgres postgres:10
+docker run --rm --name pg-docker -e POSTGRES_PASSWORD=$DB_PASSWORD -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/10/data postgres:10
