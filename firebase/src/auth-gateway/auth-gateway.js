e395459db0e3e7ec46cd630ace0e2f9dbf05fd10
Cristóvão Trevisan <cristovao.trevisan@gmail.com>
2019-05-09 23:35:19 -0300

    Fix auth gateway json support

--- firebase/src/auth-gateway/auth-gateway.js
+++ firebase/src/auth-gateway/auth-gateway.js
@@ -10,6 +10,7 @@ import { makeRhubRequest } from '../rhub'
 const app = express()
 app.use(cors)
 app.use(checkAuth(true))
+app.use(express.json())
 
 function parseRequestInfo(method, request, email, uid) {
   if (method === 'GET') {
