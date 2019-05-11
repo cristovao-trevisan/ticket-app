d0969f57d5d0e03152f6e38a0678be00e0603fb0
Cristóvão Trevisan <cristovao.trevisan@gmail.com>
2019-05-10 23:33:41 -0300

    Removing reservation routes

--- firebase/src/index.js
+++ firebase/src/index.js
@@ -3,14 +3,9 @@ import * as functions from 'firebase-functions'
 import firebase from 'firebase-admin'
 
 import authGatewayApp from './auth-gateway/auth-gateway'
-import reservationApp from './reservation/reservation'
 
 firebase.initializeApp()
 
 export const authGateway = functions.https.onRequest(authGatewayApp)
-export const reservation = functions.https.onRequest(reservationApp)
 
-if (process.env.NODE_ENV !== 'production') {
-  authGatewayApp.listen(8081)
-  reservationApp.listen(8082)
-}
+if (process.env.NODE_ENV !== 'production') authGatewayApp.listen(8081)
